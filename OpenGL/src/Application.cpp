d7fea8e603d688c95e66ac03153327ece2470b2d
Zak <github@zakburrows.co.uk>
2022-10-02 16:02:15 +0100

    Ep 11. Adding colour uniforms

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -126,6 +126,9 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // sync with monitors refresh rate
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error:" << std::endl;
 
@@ -158,16 +161,29 @@ int main(void)
 
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    GLCall(glUseProgram(shader));  
+    GLCall(glUseProgram(shader)); 
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Colour"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        GLCall(glClear(GL_COLOR_BUFFER_BIT));
-       
+        GLCall(glClear(GL_COLOR_BUFFER_BIT));       
+
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0)
+            increment = 0.05f;
+
+        r += increment;
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
