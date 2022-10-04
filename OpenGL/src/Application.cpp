003fdf98b1919dbd53879b7e79881014aaecba6b
Zak <github@zakburrows.co.uk>
2022-10-04 09:01:11 +0100

    Ep. 18 Blending theory

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -27,7 +27,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
