3917b5dd45da9a98f75a5010d44515cb348473b6
Zak <github@zakburrows.co.uk>
2022-10-04 09:21:36 +0100

    Ep 19. Maths Lib

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -12,7 +12,8 @@
 #include "Shader.h"
 #include "Texture.h"
 
-
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 int main(void)
 {
@@ -72,9 +73,12 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
+        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Colour", 0.2f, 0.3f, 0.8f, 1.0f);
+        shader.SetUniformMat4f("u_MVP", proj);
 
         Texture texture("res/textures/SpaceWallpaper.jpg");
         texture.Bind();
--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -28,7 +30,7 @@ public:
 	void SetUniform1i(const std::string& name, int i0);
 	void SetUniform1f(const std::string& name, float f0);
 	void SetUniform4f(const std::string& name, float f0, float f1, float f2, float f3);
-
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
