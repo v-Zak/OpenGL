ba6207d635b232a250869497b09d03a314e75573
Zak <github@zakburrows.co.uk>
2022-10-06 11:06:45 +0100

    Ep23. Multiple objects using uniforms

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -52,10 +52,10 @@ int main(void)
     // new scope to prevent gl terminate error infinite loop
     {
         float positions[] = {
-           100.0f, 100.0f, 0.0f, 0.0f, //0
-           200.0f, 100.0f, 1.0f, 0.0f, // 1
-            200.0f,  200.0f, 1.0f, 1.0f, // 2
-            100.0f, 200.0f, 0.0f, 1.0f, // 3
+           -50.0f, -50.0f, 0.0f, 0.0f, //0
+            50.0f, -50.0f, 1.0f, 0.0f, // 1
+            50.0f,  50.0f, 1.0f, 1.0f, // 2
+           -50.0f, 50.0f, 0.0f, 1.0f, // 3
         };
 
         unsigned int indices[] = {
@@ -77,11 +77,10 @@ int main(void)
         IndexBuffer ib(indices, 6);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
-        shader.SetUniform4f("u_Colour", 0.2f, 0.3f, 0.8f, 1.0f);
         
         Texture texture("res/textures/SpaceWallpaper.jpg");
         texture.Bind();
@@ -99,7 +98,8 @@ int main(void)
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
 
-        glm::vec3 translation(200, 200, 0);
+        glm::vec3 translationA(200, 200, 0);
+        glm::vec3 translationB(400, 200, 0);
 
         float r = 0.0f;
         float increment = 0.05f;
@@ -111,14 +111,21 @@ int main(void)
 
             ImGui_ImplGlfwGL3_NewFrame();
 
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+                glm::mat4 mvp = proj * view * model;
+                shader.Bind();
+                shader.SetUniformMat4f("u_MVP", mvp);
 
-            shader.Bind();
-            shader.SetUniform4f("u_Colour", r, 0.3f, 0.8f, 1.0f);
-            shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
-            renderer.Draw(va, ib, shader);
+            {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+                glm::mat4 mvp = proj * view * model;
+                shader.SetUniformMat4f("u_MVP", mvp);
+                renderer.Draw(va, ib, shader);
+            }
 
             if (r > 1.0f)
                 increment = -0.05f;
@@ -128,7 +135,8 @@ int main(void)
             r += increment;
 
             {
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);    
+                ImGui::SliderFloat3("TranslationA", &translationA.x, 0.0f, 960.0f);    
+                ImGui::SliderFloat3("TranslationB", &translationB.x, 0.0f, 960.0f);
                 ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             }
 
