83e722eb8f01d3dafbb0f0e6f4b6e9c1e71145fc
Zak <github@zakburrows.co.uk>
2022-10-06 09:44:10 +0100

    Ep 21. Model View Projection

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -74,14 +74,16 @@ int main(void)
         IndexBuffer ib(indices, 6);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
-        glm::vec4 result = proj * vp;
+        // in  reverse order as OpenGL is Column Major not Row Major
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Colour", 0.2f, 0.3f, 0.8f, 1.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
         
         Texture texture("res/textures/SpaceWallpaper.jpg");
         texture.Bind();
